e381413392668d81d92168d4928e9558bb823f68
Demiurgic <51731962+BikashSah999@users.noreply.github.com>
2020-08-24 01:57:23 +0545

    Update settings.py

--- Djano_Blog/settings.py
+++ Djano_Blog/settings.py
@@ -133,9 +133,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'sahbikash999@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = os.environ.get('EMAIL_USER')
+EMAIL_USE_SSL = False
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
